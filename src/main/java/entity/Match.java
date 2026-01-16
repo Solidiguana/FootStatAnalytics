d644746fbb6b54e696cc604784f5c220356e1b98
asemibragim32-ui <asemibragim32@gmail.com>
2026-01-16 18:11:47 +0500

    Match.java

--- src/main/java/entity/Match.java
+++ src/main/java/entity/Match.java
@@ -1,4 +1,34 @@
 package entity;
+import java.sql.Date;
 
-public class Match {
+public class Match extends BaseEntity {
+    private int homeTeamId;
+    private int awayTeamId;
+    private Date matchDate;
+    private String homeName;
+    private String awayName;
+
+    public Match(int id, String homeName, String awayName, Date matchDate) {
+        super(id);
+        this.homeName = homeName;
+        this.awayName = awayName;
+        this.matchDate = matchDate;
+    }
+
+    public Match(int homeTeamId, int awayTeamId, Date matchDate) {
+        super();
+        this.homeTeamId = homeTeamId;
+        this.awayTeamId = awayTeamId;
+        this.matchDate = matchDate;
+    }
+
+    public int getHomeTeamId() { return homeTeamId; }
+    public int getAwayTeamId() { return awayTeamId; }
+    public Date getMatchDate() { return matchDate; }
+
+    @Override
+    public String toString() {
+        if (homeName != null) return "ID: " + id + " | " + homeName + " vs " + awayName + " (" + matchDate + ")";
+        return "Match: " + homeTeamId + " vs " + awayTeamId;
+    }
 }
