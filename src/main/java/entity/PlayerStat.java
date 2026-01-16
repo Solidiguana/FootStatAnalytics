c9b2e0ba64493236a02db792fc46853400bb5bcb
asemibragim32-ui <asemibragim32@gmail.com>
2026-01-16 18:13:35 +0500

    PlayerStat.java

--- src/main/java/entity/PlayerStat.java
+++ src/main/java/entity/PlayerStat.java
@@ -1,4 +1,38 @@
 package entity;
 
-public class PlayerStat {
+public class PlayerStat extends BaseEntity {
+    private int playerId;
+    private int matchId;
+    private int goals;
+    private int assists;
+    private int minutes;
+    private double rating;
+
+    public PlayerStat(int id, int playerId, int matchId, int goals, int assists, int minutes, double rating) {
+        super(id);
+        this.playerId = playerId;
+        this.matchId = matchId;
+        this.goals = goals;
+        this.assists = assists;
+        this.minutes = minutes;
+        this.rating = rating;
+    }
+
+    public PlayerStat(int playerId, int matchId,
+                      int goals, int assists, int minutes, double rating) {
+        this(0, playerId, matchId, goals, assists, minutes, rating);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Stat ID: %d | Match: %d | Player: %d | Goals: %d | Assists: %d | Rating: %.2f",
+                getId(), matchId, playerId, goals, assists, rating);
+    }
+
+    public int getPlayerId() { return playerId; }
+    public int getMatchId() { return matchId; }
+    public int getGoals() { return goals; }
+    public int getAssists() { return assists; }
+    public int getMinutes() { return minutes; }
+    public double getRating() { return rating; }
 }
