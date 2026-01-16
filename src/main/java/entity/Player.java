d1ef35855da02cba3088ce41eb17022e799f7be3
asemibragim32-ui <asemibragim32@gmail.com>
2026-01-16 18:12:34 +0500

    Update Player.java

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -1,4 +1,31 @@
 package entity;
 
-public class Player {
+public class Player extends BaseEntity {
+    private String name;
+    private String position;
+    private int teamId;
+    private String teamName;
+
+    public Player(int id, String name, String position, String teamName) {
+        super(id);
+        this.name = name;
+        this.position = position;
+        this.teamName = teamName;
+    }
+
+    public Player(String name, String position, int teamId) {
+        super();
+        this.name = name;
+        this.position = position;
+        this.teamId = teamId;
+    }
+
+    public String getName() { return name; }
+    public String getPosition() { return position; }
+    public int getTeamId() { return teamId; }
+
+    @Override
+    public String toString() {
+        return String.format("ID: %-3d | %-15s | %-10s | %s", id, name, position, (teamName != null ? teamName : "TeamID: "+teamId));
+    }
 }
