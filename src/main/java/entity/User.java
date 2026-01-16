2459bb10afaa53f9c3228f1775e654f64c7685c5
asemibragim32-ui <asemibragim32@gmail.com>
2026-01-16 18:14:42 +0500

    User.java

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,4 +1,22 @@
 package entity;
 
-public class User {
+public class User extends BaseEntity {
+    private String username;
+    private String password;
+    private String role;
+
+    public User(int id, String username, String password, String role) {
+        super(id);
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User(String username, String password, String role) {
+        this(0, username, password, role);
+    }
+
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getRole() { return role; }
 }
