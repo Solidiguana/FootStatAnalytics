1cf814e3e15785b1bbeaef66690e5b9f34bd095f
asemibragim32-ui <asemibragim32@gmail.com>
2026-01-16 18:14:08 +0500

    Team.java

--- src/main/java/entity/Team.java
+++ src/main/java/entity/Team.java
@@ -1,4 +1,23 @@
 package entity;
 
-public class Team {
+public class Team extends BaseEntity {
+    private String name;
+    private String city;
+
+    public Team(int id, String name, String city) {
+        super(id);
+        this.name = name;
+        this.city = city;
+    }
+
+    public Team(String name, String city) {
+        this(0, name, city);
+    }
+
+    public String getName() { return name; }
+
+    @Override
+    public String toString() { return "ID: " + id + " | " + name + " (" + city + ")"; }
+
+    public String getCity() { return city; }
 }
