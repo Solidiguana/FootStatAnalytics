a3a0786a8da4b638d521cd4ca34521d44b739262
kuanyshmusabekuly777 <kkmusabekuly@gmail.com>
2026-01-17 14:55:46 +0500

    SecurityService
    Added hasAccess method to check user roles.

--- src/main/java/service/SecurityService.java
+++ src/main/java/service/SecurityService.java
@@ -1,4 +1,15 @@
 package service;
 
+import entity.User;
+
+public class SecurityService {
+    public static boolean hasAccess(User user, String... allowedRoles) {
+        if (user == null) return false;
+        for (String role : allowedRoles) {
+            if (user.getRole().equalsIgnoreCase(role)) return true;
+        }
+        return false;
+    }
+}
 public class SecurityService {
 }
