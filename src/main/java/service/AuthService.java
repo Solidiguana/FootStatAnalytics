f974638de1abfbc1c1a829512d7e84201454fc8d
kuanyshmusabekuly777 <kkmusabekuly@gmail.com>
2026-01-17 14:53:58 +0500

    User
    Implemented login and register methods for user authentication.

--- src/main/java/service/AuthService.java
+++ src/main/java/service/AuthService.java
@@ -1,4 +1,25 @@
 package service;
 
+import entity.User;
+import repository.UserRepository;
+
+public class AuthService {
+    private UserRepository userRepo = new UserRepository();
+
+    public User login(String username, String password) {
+        User user = userRepo.findByUsername(username);
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+        System.out.println("Invalid username or password");
+        return null;
+    }
+
+    public String register(String username, String password, String role) {
+        if (userRepo.findByUsername(username) != null) return "Login already exists";
+        User newUser = new User(username, password, role);
+        return userRepo.save(newUser) ? "Succeed" : "DB error";
+    }
+}
 public class AuthService {
 }
